6279cc678d6a96197c7da494d1e663e4640769b1
JavaClass <JavaClass@15000543cpu.fec.org>
2017-11-21 15:18:00 -0600

    3rd line

--- src/app1/NewApp.java
+++ src/app1/NewApp.java
@@ -5,7 +5,8 @@ public class NewApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("New app for Paul");// prints New app for Paul
-		System.out.println("Soon I will e coding like a master"); //prints Soon I will be coding like a master
+		System.out.println("Soon I will be coding like a master"); //prints Soon I will be coding like a master
+		System.out.println("But even masters can make mistakes"); //prints but even masters can make mistakes
 	}
 
 }
