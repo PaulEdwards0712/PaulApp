4a425715a435c557c023ad2a9a753812e232471b
JavaClass <JavaClass@15000543cpu.fec.org>
2017-11-21 15:12:40 -0600

    master of disaster

--- src/app1/NewApp.java
+++ src/app1/NewApp.java
@@ -5,6 +5,7 @@ public class NewApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("New app for Paul");// prints New app for Paul
+		System.out.println("Soon I will e coding like a master"); //prints Soon I will be coding like a master
 	}
 
 }
